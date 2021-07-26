71953848483334e2e80b4261f9a62c92fb45fe02
Meetkumar Patel <87077346+Meet2020mp@users.noreply.github.com>
2021-07-27 00:48:12 +0530

    Add files via upload

---
+++ CSI Project/footer.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style>
+      body{ height:100vh; margin:0; }
+
+      /* footer{ min-height:35px; background:#a6a6a6; } */
+
+      body{ 
+        display:flex; 
+        flex-direction:column; 
+      }
+
+      footer{
+        margin-top:auto; 
+        background-color:#a6a6a6;
+        text-align: center;
+        align-items:center;
+        justify-content:center;
+        padding-top:5px;
+        padding-bottom:8px;
+        width:100%;
+        font-size:16px;
+      }
+        footer:hover {
+          background-color: #212725;
+          color: #ffffff;
+          text-align: center;
+          /* font-size: 16px; */
+          /* padding: 12px; */
+        }
+    </style>
+</head>
+<body>
+    
+</body>
+
+  <footer contentEditable>Copyright &copy; 2020-21 | To-Do CSI</footer>
+</html>
---
+++ project.sql
@@ -0,0 +1,89 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 26, 2021 at 09:10 PM
+-- Server version: 10.4.17-MariaDB
+-- PHP Version: 8.0.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `project`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `notes`
+--
+
+CREATE TABLE `notes` (
+  `sno` int(11) NOT NULL,
+  `title` varchar(50) NOT NULL,
+  `description` text NOT NULL,
+  `timestmp` datetime NOT NULL DEFAULT current_timestamp(),
+  `user_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `notes`
+--
+
+INSERT INTO `notes` (`sno`, `title`, `description`, `timestmp`, `user_id`) VALUES
+(1, 'Mathematics', 'Statistics and Probability', '0000-00-00 00:00:00', 0),
+(2, 'Programming', 'Data structures and algorithms and AI.', '0000-00-00 00:00:00', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `username` varchar(20) NOT NULL,
+  `email` varchar(20) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `dt` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`username`, `email`, `password`, `dt`) VALUES
+('Meetkumar', 'abc@gmail.com', '$2y$10$MicJSFMGdDDemwLx88GG5.8rv1SG11wcwm7FcKdlNEOEbYmeoxicm', '2021-07-27 00:34:16');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `notes`
+--
+ALTER TABLE `notes`
+  ADD PRIMARY KEY (`sno`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `notes`
+--
+ALTER TABLE `notes`
+  MODIFY `sno` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
